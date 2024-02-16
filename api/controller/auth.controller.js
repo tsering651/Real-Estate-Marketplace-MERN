b4a1bbdba5cac9ffae8cef9214f893741390c85b
tsering651 <85874266+tsering651@users.noreply.github.com>
2024-02-16 10:40:01 +0530

    Encrypted password

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,10 +1,17 @@
 import User from "../models/user.model.js";
+import bcrypt from 'bcryptjs';
 
 
 export const signup= async (req,res)=>{
    const {username,email,password}=req.body;
-   const newUser=new User({username,email,password});
-   await newUser.save();
-   res.status(201).json("user created succesfully");
+   const hashPassword=bcrypt.hashSync(password,10);
+   const newUser=new User({username,email,password:hashPassword});
+   try {
+      await newUser.save();
+     res.status(201).json("user created succesfully");
+   } catch (error) {
+      res.status(500).json(error.message);
+   }
+   
 }
 
