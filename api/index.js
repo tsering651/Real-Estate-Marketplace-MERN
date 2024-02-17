a426ef55119c2ce6dafdf502171cec5a756b4c19
tsering651 <85874266+tsering651@users.noreply.github.com>
2024-02-17 17:39:10 +0530

    Done with the signup api

--- api/index.js
+++ api/index.js
@@ -1,8 +1,11 @@
 import express from 'express'
+
 import mongoose from 'mongoose';
+import cors from 'cors';
 import dotenv from 'dotenv'
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
+
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("Connected to the DB");
@@ -11,6 +14,7 @@ mongoose.connect(process.env.MONGO).then(()=>{
     console.log(err);
 })
 const app=express();
+app.use(cors());
 app.use(express.json());
 
 app.listen(3000,()=>{
--- customer/src/pages/SignUp.jsx
+++ customer/src/pages/SignUp.jsx
@@ -1,7 +1,79 @@
-import React from 'react'
+import  { useState } from 'react';
+import '../styles/sign-up.css'; // Import your CSS file
 
-export default function SignUp() {
+function App() {
+  
+  const [formData,setformData]=useState({});
+
+  const handleChange = (e)=>{
+          setformData({
+             ...formData,
+             [e.target.id]:e.target.value,
+          })
+  }
+  const handleSubmit= async (e)=>{
+    e.preventDefault();
+    const res=await fetch('/api/auth/signup',
+    {
+      method:'POST',
+      headers:{
+        'content-Type':'application/json',
+      },
+      body:JSON.stringify(formData),
+    }
+    );
+    const data=await res.json();
+    console.log(data);
+
+     
+
+  }
+ // console.log(formData);
+
   return (
-    <div>SignUp</div>
-  )
+    <div className="signup-container">
+      <h1>Sign Up</h1>
+      
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+         
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+         
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="confirmPassword">Confirm Password:</label>
+        <input
+          type="password"
+          id="confirmPassword"
+          name="confirmPassword"
+        
+          onChange={handleChange}
+          required
+        />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
 }
+
+export default App;
