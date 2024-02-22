f5f1357a91327e6b8132fe04f610aed94a23d467
Vikash <vkvermavk96@gmail.com>
2024-02-22 09:40:58 +0530

    design

--- customer/src/pages/Home.jsx
+++ customer/src/pages/Home.jsx
@@ -1,16 +1,26 @@
-import { useState } from "react";
-import { AiOutlineMail, AiOutlineLock } from "react-icons/ai";
-import { Link, useNavigate } from "react-router-dom";
+import videoback from "./images/video.mp4"
 import backgroundImage from "./images/Property-.jpg";
 
 export default function Home() {
   return (
     <div
-      className="bg-cover bg-center min-h-screen flex items-center justify-center "
-      style={{
-        backgroundImage: `url(${backgroundImage})`,
-      }}
+      // className="bg-cover bg-center min-h-screen flex items-center justify-center "
+      // style={{
+      //   backgroundImage: `url(${backgroundImage})`,
+      // }}
     > 
+     
+     <video
+        className="absolute inset-0 w-full h-full object-cover"
+        autoPlay
+        loop
+        muted
+      >
+     <source src={videoback} type="video/mp4" />
+      </video>
+        <div className="absolute w-full h-full flex justify-center items-center text-black font-bold">
+        <h1>Welcome to my website</h1>
+      </div>
     </div>
   );
 }
