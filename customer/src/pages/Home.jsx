3cf1ced8e5f04246c07a97b3b0077ff1049c4472
Vikash <vkvermavk96@gmail.com>
2024-02-26 16:22:41 +0530

    some changes

--- customer/src/pages/Home.jsx
+++ customer/src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import videoback from "./images/video.mp4"
 import backgroundImage from "./images/Property-.jpg";
 
+
 export default function Home() {
   return (
     <div
-      // className="bg-cover bg-center min-h-screen flex items-center justify-center "
-      // style={{
-      //   backgroundImage: `url(${backgroundImage})`,
-      // }}
+      className="bg-cover bg-center min-h-screen flex items-center justify-center "
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+      }}
     > 
      
      <video
@@ -18,9 +19,6 @@ export default function Home() {
       >
      <source src={videoback} type="video/mp4" />
       </video>
-        <div className="absolute w-full h-full flex justify-center items-center text-black font-bold">
-        <h1>Welcome to my website</h1>
-      </div>
     </div>
   );
 }
