d4137c226b17ada20d20b175447cbedcc1b798e3
tsering651 <85874266+tsering651@users.noreply.github.com>
2024-03-05 18:13:29 +0530

    Created get user listing API route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { updatedUser,deleteUser } from '../controller/user.controller.js';
+import { updatedUser,deleteUser,getUserListings } from '../controller/user.controller.js';
 import {verifyToken} from '../utils/verifyUser.js'
 
 
@@ -9,5 +9,7 @@ const router = express.Router();
 router.post('/update/:id',verifyToken,updatedUser )
 
 router.delete('/delete/:id',verifyToken,deleteUser )
+
+router.get('/listings/:id',verifyToken,getUserListings)
 
 export default router;
