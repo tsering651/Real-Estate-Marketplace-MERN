bda403e1f59fde5579499284bede4b78d782518f
Vikash <vkvermavk96@gmail.com>
2024-03-06 23:26:36 +0530

    somechanges and new images and reduced app size

--- customer/src/pages/Home.jsx
+++ customer/src/pages/Home.jsx
@@ -5,7 +5,7 @@ import backgroundImage from "./images/home.jpg";
 export default function Home() {
   return (
     <div
-      className="bg-cover bg-center min-h-screen flex items-center justify-center "
+      className="bg-cover bg-bottom min-h-screen flex items-center justify-center "
       style={{
         backgroundImage: `url(${backgroundImage})`,
       }}
