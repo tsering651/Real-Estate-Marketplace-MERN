d5f7ac6e87d6fc3d46eb0ac837e77aaec3420331
Vikash <vkvermavk96@gmail.com>
2024-04-10 09:12:29 +0530

    hey

--- customer/src/pages/Listing.jsx
+++ customer/src/pages/Listing.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Navigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore from "swiper";
 import { Navigation } from "swiper/modules";
