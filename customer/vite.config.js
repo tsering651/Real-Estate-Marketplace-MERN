fed454b3548b32f01c9402f8632d87a58ce88781
tsering651 <85874266+tsering651@users.noreply.github.com>
2024-04-25 15:35:15 +0530

    url changes

--- customer/vite.config.js
+++ customer/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target:'http://localhost:3000/',
+        target:'https://real-estate-marketplace-mern-5.onrender.com',
         secure:false,
       },
     },
